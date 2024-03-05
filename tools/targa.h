c9d8d6ea578c4bba251005a6350306709e1c82ad
bencehari <bencehari@gmail.com>
2024-03-05 16:51:10 +0100

    targa footer, WiP (viewers are crashing at open)

--- tools/targa.h
+++ tools/targa.h
@@ -8,17 +8,31 @@
 extern "C" {
 #endif
 
+enum ColorDataStart {
+	BOTTOM_LEFT,
+	BOTTOM_RIGHT,
+	TOP_LEFT,
+	TOP_RIGHT
+};
+
 /**
  * @brief Creates targa image file.
  *
  * @param _fileName Name of the output file without extension.
  * @param _width Width of image.
  * @param _height Height of image.
+ * @param _start Starting corner of color data.
+ * @param _colors Color data.
  * @param _verify Verifies the output file.
- *
  * @return False on error, otherwise true.
 */
-extern bool create_tga_image(const char* _fileName, const uint16_t _width, const uint16_t _height, const bool _verify);
+extern bool create_tga_image(
+	const char* _fileName,
+	const uint16_t _width,
+	const uint16_t _height,
+	const enum ColorDataStart _start,
+	const uint8_t* _colors,
+	const bool _verify);
 
 #ifdef __cplusplus
 }
